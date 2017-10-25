c0cf1e65950ae12fabd9d3752004f8302d7992e6
Kamyl <kamillcybulski@gmail.com>
2017-10-26 01:03:07 +0200

    Add DB connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+
+const User = require('./models/user');
 
 const app = express();
 app.use(express.static('client/build'));
 
+mongoose.connect(process.env.YKJS_2_DB);
 
-app.get('/api/test', (req, res) => {
-  res.json({ yo: 'yoyoyo' });
+app.get('/api/test', async (req, res) => {
+  await User.create({ username: 'Snoopy' });
+  const data = await User.findOne({});
+
+  res.json(data);
 });
 
 app.get('/*', (req, res) => {
